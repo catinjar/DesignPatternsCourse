8290d6c6da7238e0cd9408ef2aeaa5f02d92ad69
Denis Vyatkin <catinjar42@gmail.com>
2019-11-03 12:23:47 +0500

    Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3 {
+    public class EmailLetter {
+        public List<string> Receivers { get; set; } = new List<string>();
+        public string Body { get; set; }
+        public string Theme { get; set; }
+
+        public void Print() {
+            Console.WriteLine($"Receivers: {string.Join(", ", Receivers)}");
+            if (!string.IsNullOrEmpty(Theme)) {
+                Console.WriteLine($"Theme: {Theme}");
+            }
+            Console.WriteLine($"Body: {Body}");
+        }
+    }
+
+    public class EmailBuilder {
+        private EmailLetter letter = new EmailLetter();
+
+        public EmailBuilder AddReceiver(string receiver) {
+            letter.Receivers.Add(receiver);
+            return this;
+        }
+
+        public EmailBuilder SetBody(string body) {
+            letter.Body = body;
+            return this;
+        }
+
+        public EmailBuilder SetTheme(string theme) {
+            letter.Theme = theme;
+            return this;
+        }
+
+        public EmailLetter Result {
+            get {
+                if (letter.Receivers.Count == 0) {
+                    throw new FormatException("The letter must have at least one receiver!");
+                }
+
+                if (string.IsNullOrEmpty(letter.Body)) {
+                    throw new FormatException("The letter must have a body!");
+                }
+
+                return letter;
+            }
+        }
+    }
+
+    class Program {
+        private static void Main(string[] args) {
+            var emailBuilder = new EmailBuilder();
+
+            var letter = emailBuilder
+                .AddReceiver("someone@gmail.com")
+                .AddReceiver("anotherone@gmail.com")
+                .SetBody("Hello!")
+                .SetTheme("This is a test")
+                .Result;
+
+            letter.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
